86c64e3da416fd8894b62b572a06653b96186f7b
AleBez85 <alebez85@gmail.com>
2023-06-04 20:39:17 +0300

    Added a calculation of the time how long the task was in operation and testing.

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -1,18 +1,26 @@
 package com.javarush.jira.bugtracking;
 
 import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
+import com.javarush.jira.bugtracking.internal.model.Activity;
 import com.javarush.jira.bugtracking.internal.model.Task;
+import com.javarush.jira.bugtracking.internal.repository.ActivityRepository;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
-    public TaskService(TaskRepository taskRepository, TaskMapper mapper) {
+    private final ActivityRepository activityRepository;
+
+    public TaskService(TaskRepository taskRepository, TaskMapper mapper, ActivityRepository activityRepository) {
         super(taskRepository, mapper);
+        this.activityRepository = activityRepository;
     }
 
     public List<TaskTo> getAll() {
@@ -35,5 +43,27 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
         Task task = repository.getExisted(id);
         task.setStatusCode(statusCode);
         repository.save(task);
+    }
+
+    public Duration taskWasInProgress(Long taskId) {
+        Optional<Activity> inProgress = activityRepository.findByIdAndStatusCode(taskId, "in progress");
+        Optional<Activity> ready = activityRepository.findByIdAndStatusCode(taskId, "ready");
+        if (inProgress.isPresent() && ready.isPresent()) {
+            LocalDateTime localDateTime1 = inProgress.get().getUpdated();
+            LocalDateTime localDateTime2 = ready.get().getUpdated();
+            return Duration.between(localDateTime1, localDateTime2);
+        }
+        return null;
+    }
+
+    public Duration taskWasInTesting(Long taskId) {
+        Optional<Activity> ready = activityRepository.findByIdAndStatusCode(taskId, "ready");
+        Optional<Activity> testing = activityRepository.findByIdAndStatusCode(taskId, "testing");
+        if (ready.isPresent() && testing.isPresent()) {
+            LocalDateTime localDateTime1 = ready.get().getUpdated();
+            LocalDateTime localDateTime2 = testing.get().getUpdated();
+            return Duration.between(localDateTime1, localDateTime2);
+        }
+        return null;
     }
 }
--- src/main/java/com/javarush/jira/bugtracking/internal/repository/ActivityRepository.java
+++ src/main/java/com/javarush/jira/bugtracking/internal/repository/ActivityRepository.java
@@ -4,6 +4,9 @@ import com.javarush.jira.bugtracking.internal.model.Activity;
 import com.javarush.jira.common.BaseRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface ActivityRepository extends BaseRepository<Activity> {
+    Optional<Activity> findByIdAndStatusCode(Long id, String statusCode);
 }
